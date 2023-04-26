8d06db18f2d0a68b8682ae94970889f8fbfffed9
Shai Tayar <47159139+shaitayar@users.noreply.github.com>
2023-04-26 12:01:01 +0300

    after update

--- bp.cpp
+++ bp.cpp
@@ -5,6 +5,7 @@
 #include <cmath>
 #include <vector>
 #include <map>
+#include <iostream>
 
 using std::vector;
 using std::map;
@@ -31,7 +32,9 @@ public:
 
     State getPredict() { return predict; }
 
-    void setPredict(bool isTaken) {
+    void setPredict(State pred) { predict = pred; }
+
+    void UpdatePredict(bool isTaken) {
         switch (predict) {
             case SNT:
                 predict = isTaken ? WNT : SNT;
@@ -68,39 +71,44 @@ private:
     vector<uint32_t> Targets;
     // Local
     vector<uint32_t> LocalHistories;
-    vector<vector<FSM*>> LocalTables;
+    vector<vector<FSM *>> LocalTables;
     // Global
     uint32_t GlobalHistory;
-    vector<FSM*> GlobalTable;
+    vector<FSM *> GlobalTable;
     int HT = -1;
     SIM_stats status;
 
     //return value -  The theoretical size of the TBT
-    uint32_t calcTheoreticalSize();
+    uint32_t calcSize();
 
 public:
     BP() = default;
+
     void init(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned fsmState,
-       bool isGlobalHist, bool isGlobalTable, int Shared);
-    BP(BP const&) = delete; // disable copy ctor
-    void operator=(BP const&) = delete; // disable = operator
-    ~BP();
-    static BP& getInstance() // make singleton
+              bool isGlobalHist, bool isGlobalTable, int Shared);
+
+    BP(BP const &) = delete; // disable copy ctor
+    void operator=(BP const &) = delete; // disable = operator
+    ~BP() = default;
+
+    static BP &getInstance() // make singleton
     {
         static BP instance; // Guaranteed to be destroyed.
         // Instantiated on first use.
         return instance;
     }
 
+    int getTag(uint32_t pc);
 
-/***
- * Predict - this function predicts if branch is taken or not taken
- * @param pc - the branch instruction's pointer.
- * @param dst - the destination for the predicted action.
- *                 pc+4 if NT and target if taken.
- * @return prediction - false if NOT TAKEN, true if TAKEN
- */
+    int getIndex(uint32_t pc);
+
     bool Predict(uint32_t pc, uint32_t *dst);
+
+    void fillStatus(SIM_stats *curStats);
+
+    void erase();
+
+    void Update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst);
 };
 
 
@@ -109,34 +117,31 @@ public:
  **********************************************/
 
 
-uint32_t BP::calcTheoreticalSize()
-{
-    uint32_t size = btbSize * (tagSize + ADDRESS_SIZE);
+uint32_t BP::calcSize() {
+    uint32_t size = btbSize * (1+tagSize + ADDRESS_SIZE);
     uint32_t globalHistSize = historySize;
     uint32_t localHistSize = btbSize * historySize;
     uint32_t globalTblSize = pow(2, historySize) * BIMODAL_SIZE;
     uint32_t localTblSize = btbSize * pow(2, historySize) * BIMODAL_SIZE;
-    switch (HT)
-    {
-    //both local
-    case 0:
-    return size + localHistSize + localTblSize;
+    switch (HT) {
+        //both local
+        case 0:
+            return size + localHistSize + localTblSize;
 
-    //Hist local Table global
-    case 1:
-    return size + localHistSize + globalTblSize;
+            //Hist local Table global
+        case 1:
+            return size + localHistSize + globalTblSize;
 
-    //Hist Global Table local
-    case 10:
-    return size + globalHistSize + localTblSize;
+            //Hist Global Table local
+        case 10:
+            return size + globalHistSize + localTblSize;
 
-    //both global
-    case 11:
-    return size + globalHistSize + globalTblSize;
+            //both global
+        case 11:
+            return size + globalHistSize + globalTblSize;
     }
     return 0;
 }
-
 
 
 /**********************************************
@@ -145,74 +150,69 @@ uint32_t BP::calcTheoreticalSize()
 
 
 void BP::init(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned fsmState,
-       bool isGlobalHist, bool isGlobalTable, int Shared)
-{
+              bool isGlobalHist, bool isGlobalTable, int Shared) {
     this->btbSize = btbSize;
     this->historySize = historySize;
-    this->tagSize= tagSize;
+    this->tagSize = tagSize;
     this->fsmState = static_cast<State>(fsmState);
     this->isHistGlobal = isHistGlobal;
     this->isTableGlobal = isTableGlobal;
     this->isShare = isShare;
+
     //Tags and Target init
-        for (int i = 0; i < btbSize; ++i)
-        {
-            Tags.push_back(0);
-            Targets.push_back(0);
+    for (unsigned i = 0; i < btbSize; ++i) {
+        Tags.push_back(0);
+        Targets.push_back(0);
+    }
+
+    //History init
+    if (isHistGlobal) {
+        GlobalHistory = 0;
+    } else {
+        for (unsigned i = 0; i < btbSize; ++i) {
+            LocalHistories.push_back(0);
         }
-        //History init
-        if (isHistGlobal)
-        {
-            GlobalHistory = 0;
+    }
+
+    //Table init
+    if (isTableGlobal) {
+        for (unsigned i = 0; i < pow(2, historySize); ++i) {
+            GlobalTable.push_back(new FSM(this->fsmState));
         }
-        else
-        {
-            for (int i = 0; i < btbSize; ++i) {
-                LocalHistories.push_back(0);
+    }
+
+    else {
+        for (unsigned i = 0; i < btbSize; ++i) {
+            vector<FSM * > v1;
+            for (int j = 0; j < pow(2, historySize); ++j) {
+                v1.push_back(new FSM(this->fsmState));
+                //LocalTables[i].push_back((new FSM(this->fsmState)));
             }
+            LocalTables.push_back(v1);
         }
-        //Table init
-        if (isTableGlobal)
-        {
-            for (int i = 0; i < pow(2, historySize); ++i) {
-                GlobalTable.push_back(new FSM(this->fsmState));
-            }
-        }
-        else
-        {
-            for (int i = 0; i < btbSize; ++i)
-            {
-                for (int j = 0; j < pow(2, historySize); ++j)
-                {
-                    LocalTables[i].push_back(new FSM(this->fsmState));
-                }
-            }
-        }
-        HT = int(isGlobalHist) * 10 + int(isTableGlobal);
-        //Init Status
-        status.size = calcTheoreticalSize();
-        status.br_num = 0;
-        status.flush_num = 0;
+    }
+
+
+    HT = int(isGlobalHist) * 10 + int(isTableGlobal);
+    //Init Status
+    status.size = calcSize();
+    status.br_num = 0;
+    status.flush_num = 0;
+
 };
 
 
-BP::~BP()
-{
-    if (!LocalTables.empty())
-    {
-        for (vector<FSM*> table : LocalTables)
-        {
-            if (!table.empty())
-            {
-                for (FSM* FSM : table)
-                {
+void BP::erase() {
+    if (!LocalTables.empty()) {
+        for (vector<FSM *> table : LocalTables) {
+            if (!table.empty()) {
+                for (FSM *FSM : table) {
                     delete FSM;
                 }
             }
         }
     }
-    if (!GlobalTable.empty())
-    {
+    if (!GlobalTable.empty()) {
         for (FSM *FSM: GlobalTable) {
             delete FSM;
         }
@@ -220,28 +220,129 @@ BP::~BP()
 }
 
 
-bool BP::Predict(uint32_t pc, uint32_t *dst)
-{
-    //TODO: add methods for each case and update status.br_num
-    switch (HT) {
-    //both local
-    case 0:
+int BP::getTag(uint32_t pc) {
+    int shift = log2((double) (btbSize)) + 2;
+    unsigned mask = ((1 << tagSize) - 1) << shift;
+    return (pc & mask) >> shift;
+}
 
-    break;
-    //Hist local Table global
-    case 1:
 
-    break;
-    //Hist Global Table local
-    case 10:
+int BP::getIndex(uint32_t pc) {
+    int bitSizeOfBTB = log2((double) (btbSize));
+    unsigned mask = ((1 << bitSizeOfBTB) - 1) << 2;
+    return (pc & mask) >> 2;
+}
 
-    break;
-    //both global
-    case 11:
 
-    break;
+bool BP::Predict(uint32_t pc, uint32_t *dst) {
+    unsigned tag = getTag(pc);
+    int index = getIndex(pc);
+    if (Tags[index] != tag) {
+        *dst = pc + 4;
+        return false;
     }
-    return true;
+    if (isHistGlobal) {
+        if (isTableGlobal) {
+            if (GlobalTable[GlobalHistory]->getPredict() == ST ||
+                GlobalTable[GlobalHistory]->getPredict() == SNT) {
+                *dst = pc + 4;
+                return false;
+            } else {
+                *dst = Targets[index];
+                return true;
+            }
+        } else {
+            //TODO: check if meant to write WNT instead of ST
+            if (LocalTables[index][GlobalHistory]->getPredict() == ST ||
+                LocalTables[index][GlobalHistory]->getPredict() == SNT) {
+                *dst = pc + 4;
+                return false;
+            } else {
+                *dst = Targets[index];
+                return true;
+            }
+        }
+    } else {
+        if (isTableGlobal) {
+            //TODO: SAME as ABOVE
+            if (GlobalTable[LocalHistories[index]]->getPredict() == ST ||
+                GlobalTable[LocalHistories[index]]->getPredict() == SNT) {
+                *dst = pc + 4;
+                return false;
+            } else {
+                *dst = Targets[index];
+                return true;
+            }
+        } else {
+            //TODO: SAME as ABOVE
+            if (LocalTables[index][LocalHistories[index]]->getPredict() == ST ||
+                LocalTables[index][LocalHistories[index]]->getPredict() == SNT) {
+                *dst = pc + 4;
+                return false;
+            } else {
+                *dst = Targets[index];
+                return true;
+            }
+        }
+    }
+}
+
+
+void BP::Update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst) {
+    status.br_num++;
+    if ((!taken && pred_dst != pc + 4) || (taken && pred_dst != targetPc)) status.flush_num++;
+
+
+    unsigned tag = getTag(pc);
+    int index = getIndex(pc);
+
+    bool isOverride = (Tags[index] != tag);
+
+    if (isOverride) Targets[index] = targetPc;
+
+    switch (HT) {
+        //both local
+        case 0:
+            isOverride ? LocalHistories[index] = taken : LocalHistories[index] <<= 1;
+            LocalHistories[index] |= uint32_t (taken);
+            isOverride ? LocalTables[LocalHistories[index]][index]->setPredict(fsmState)
+                       : LocalTables[LocalHistories[index]][index]->UpdatePredict(taken);
+            break;
+            //Hist local Table global
+        case 1:
+            isOverride ? LocalHistories[index] = taken : LocalHistories[index] <<= 1;
+            LocalHistories[index] |= uint32_t (taken);
+            isOverride ? GlobalTable[LocalHistories[index]]->setPredict(fsmState)
+                       : GlobalTable[LocalHistories[index]]->UpdatePredict(taken);
+            break;
+            //Hist Global Table local
+        case 10:
+            GlobalHistory <<= 1; //todo: check if can be override
+            GlobalHistory |= uint32_t (taken);
+            isOverride ? LocalTables[index][GlobalHistory]->setPredict(fsmState)
+                       : LocalTables[index][GlobalHistory]->UpdatePredict(taken);
+
+            break;
+            //both global
+        case 11:
+            GlobalHistory <<= 1; //todo: check if can be override
+            GlobalHistory |= uint32_t (taken);
+            GlobalTable[GlobalHistory]->UpdatePredict(taken); //todo: check if can be override
+            break;
+    }
+}
+
+
+/***
+ * GetStatus - returns the Simulator's status in curStats
+ * @param curStats - pointer to return the Simulator's stat.
+ */
+
+void BP::fillStatus(SIM_stats *curStats) {
+    (*curStats).size = status.size;
+    (*curStats).br_num = status.br_num;
+    (*curStats).flush_num = status.flush_num;
+    return;
 }
 
 
@@ -249,52 +350,31 @@ bool BP::Predict(uint32_t pc, uint32_t *dst)
  * Main Functions
  **********************************************/
 
-
-/***
- * Update - update predictor with actual decision
- * @param pc - the branch instruction's pointer.
- * @param targetPc - the branch instruction's pointer.
- */
-    void Update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst) {
-
-    }
-/***
- * GetStatus - returns the Simulator's status in curStats
- * @param curStats - pointer to return the Simulator's stat.
- */
-    void GetStatus(SIM_stats *curStats)
-{
-    //{ *curStats = status; }
-
-};
-
-/****** GIVEN FUNCTIONS ******/
-
 int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned fsmState,
             bool isGlobalHist, bool isGlobalTable, int Shared) {
-    BP& bp = BP::getInstance();
-    if (&bp)
-    {
+
+    BP &bp = BP::getInstance();
+    if (&bp == nullptr) {
         return -1;
     }
     bp.init(btbSize, historySize, tagSize, fsmState, isGlobalHist, isGlobalTable, Shared);
     return 0;
 }
 
-bool BP_predict(uint32_t pc, uint32_t *dst)
-{
-    BP& bp = BP::getInstance();
-    int mask = 1 >> bp
-    int Instruction_tag = pc <<
-    return false;
+
+bool BP_predict(uint32_t pc, uint32_t *dst) {
+    BP &bp = BP::getInstance();
+    return bp.Predict(pc, dst);
 }
 
+
 void BP_update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst) {
-    return;
+    BP &bp = BP::getInstance();
+    bp.Update(pc, targetPc, taken, pred_dst);
 }
 
 void BP_GetStats(SIM_stats *curStats) {
-    return;
+    BP &bp = BP::getInstance();
+    bp.fillStatus(curStats);
+    bp.erase();
 }
-
-
